7853b9489a6320e9708cf55e51624c713d717787
Gunasekar Jabbala <gunasekar.ai.dev@gmail.com>
2023-06-12 23:49:39 +0530

    Commiting cppnd capstone project

---
+++ Capstone Project/build/CMakeFiles/tictactoe.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/workspace/tictactoe/src/field.cpp" "/home/workspace/tictactoe/build/CMakeFiles/tictactoe.dir/src/field.cpp.o"
+  "/home/workspace/tictactoe/src/main.cpp" "/home/workspace/tictactoe/build/CMakeFiles/tictactoe.dir/src/main.cpp.o"
+  "/home/workspace/tictactoe/src/sprite.cpp" "/home/workspace/tictactoe/build/CMakeFiles/tictactoe.dir/src/sprite.cpp.o"
+  "/home/workspace/tictactoe/src/window.cpp" "/home/workspace/tictactoe/build/CMakeFiles/tictactoe.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Capstone Project/build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/src/field.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/main.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/sprite.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/window.cpp.o"
+  "tictactoe.pdb"
+  "tictactoe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
